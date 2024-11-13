55ef7a143a2cfdbfe6e8edb88c6fd731cd9eef0c
Matheus Mendes <matheuskindrow@gmail.com>
2024-11-13 08:35:06 -0300

    bruh

--- main.py
+++ main.py
@@ -16,14 +16,23 @@ class Cliente(BaseModel):
     email: str
     telefone: str
 
+    def to_dataframe(self):
+        return pd.DataFrame({"id": self.id,
+                            "nome": self.nome,
+                            "sexo": self.sexo,
+                            "endereco": self.endereco,
+                            "email": self.email,
+                            "telefone": self.telefone})
+
 
 clientes = pd.read_csv('clientes.csv', dtype={'id': 'Int32', 'nome': 'str', 'sexo': 'str', 'endereco': 'str', 'email': 'str', 'telefone': 'str'})
 
 
+
 @app.post("/clientes/", response_model=Cliente, status_code=HTTPStatus.CREATED)
 def adicionar_cliente(cliente: Cliente):
-    if any(cliente_atual.id == cliente.id for label, cliente_atual in clientes.items()):
+    if any(cliente_atual["id"] == cliente.id for index, cliente_atual in clientes.iterrows()):
         raise HTTPException(status_code=400, detail="ID já existe.")
-    clientes.append(cliente)
+    pd.concat([clientes, cliente.to_dataframe()], index="id")
     clientes.to_csv("clientes.csv")
     return cliente
